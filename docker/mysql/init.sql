47a2117d4a022bf321fbbe9cb753bd422d8f6c75
Raouf Medjdoub <raouf.medjdoub@gmail.com>
2026-03-18 09:29:06 +0100

    fix: grant root access from Docker network

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,8 +1,9 @@
--- Bases de données GPSWOX
+-- Autoriser root depuis n'importe quelle IP Docker
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Woai6O4NkCjM7AEWvAVlVgDds9jR0OV6';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+-- Créer les bases de données
 CREATE DATABASE IF NOT EXISTS gpswox_web CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE DATABASE IF NOT EXISTS gpswox_traccar CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
--- Permissions utilisateur gpswox sur les deux bases
-GRANT ALL PRIVILEGES ON gpswox_web.* TO 'gpswox'@'%';
-GRANT ALL PRIVILEGES ON gpswox_traccar.* TO 'gpswox'@'%';
 FLUSH PRIVILEGES;
