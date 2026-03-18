0ee48fc2767c779e2d34005d0053feb79bef0f07
Raouf Medjdoub <raouf.medjdoub@gmail.com>
2026-03-18 09:07:32 +0100

    fix: two databases gpswox_web + gpswox_traccar

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,4 +1,8 @@
--- Création de la base de données GPSWOX si elle n'existe pas
-CREATE DATABASE IF NOT EXISTS gpswox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON gpswox.* TO 'gpswox'@'%';
+-- Bases de données GPSWOX
+CREATE DATABASE IF NOT EXISTS gpswox_web CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS gpswox_traccar CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+-- Permissions utilisateur gpswox sur les deux bases
+GRANT ALL PRIVILEGES ON gpswox_web.* TO 'gpswox'@'%';
+GRANT ALL PRIVILEGES ON gpswox_traccar.* TO 'gpswox'@'%';
 FLUSH PRIVILEGES;
